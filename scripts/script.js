25d8010595224afafce88487322ee860de1b7186
Georgiy Kuvykin <gedrargerum4@gmail.com>
2020-08-25 02:37:32 +0300

    fix ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,34 +1,34 @@
 let profileName = document.querySelector(".profile__name");
 let profileDescription = document.querySelector(".profile__description");
 let popup = document.querySelector(".popup");
+let form = popup.querySelector(".form");
 let openButton = document.querySelector(".profile__edit-button");
 let closeButton = popup.querySelector(".form__close");
-let inputName = popup.querySelectorAll(".form__input");
-let saveButton = popup.querySelector(".form__button")
+let name = popup.querySelector(".form__name");
+let description = popup.querySelector(".form__description");
+let inputName = [name, description];
+let saveButton = popup.querySelector(".form__button");
 
-console.log(inputName);
 
 
 let closePopup = function (event) {
     if (event.target !== event.currentTarget) return
-    popuoToggle();
+    popupToggle();
 
 }
 
 
-let popuoToggle = function () {
+let popupToggle = function () {
     popup.classList.toggle("popup_display-on");
+        name.value = profileName.textContent;
+        description.value = profileDescription.textContent
 }
 
-let saveWithEnter = function (e) {
-    if (popup.classList.contains("popup_display-on") === true) {
-        if (e.keyCode == "13") {
-            getPopupValues();
-        }
-    }
-}
 
-let getPopupValues = function () {
+
+let getPopupValues = function (event) {
+    event.preventDefault()
+
     let headerInfo = {
         'name': profileName,
         'description': profileDescription,
@@ -37,12 +37,11 @@ let getPopupValues = function () {
     for (let i = 0;  i < inputName.length; i++) {
         headerInfo[inputName[i].name].textContent = inputName[i].value;
     }
-    popuoToggle();
+    popupToggle();
 
 }
 
-addEventListener("keydown", saveWithEnter);
-saveButton.addEventListener('click', getPopupValues);
-openButton.addEventListener('click', popuoToggle);
-closeButton.addEventListener('click', popuoToggle);
+form.addEventListener('submit', getPopupValues);
+openButton.addEventListener('click', popupToggle);
+closeButton.addEventListener('click', popupToggle);
 popup.addEventListener('click', closePopup)

35eaf1173cd50ab28d5009430870831fadb5e317
Georgiy Kuvykin <gedrargerum4@gmail.com>
2020-09-03 08:53:33 +0300

    feature : добалено новый попап и js

--- scripts/script.js
+++ scripts/script.js
@@ -1,48 +1,119 @@
-let profileName = document.querySelector(".profile__name");
-let profileDescription = document.querySelector(".profile__description");
-let popup = document.querySelector(".popup");
-let form = popup.querySelector(".form");
-let openButton = document.querySelector(".profile__edit-button");
-let closeButton = popup.querySelector(".form__close");
-let name = popup.querySelector(".form__name");
-let description = popup.querySelector(".form__description");
-let inputName = [name, description];
-let saveButton = popup.querySelector(".form__button");
+
+// список элементов картинок
+const elements = document.querySelector(".elements");
+
+// кнопка редактирования профиля
+const openButton = document.querySelector(".profile__edit-button");
+
+// кнопка добавления фото
+const addButton = document.querySelector(".profile__add-button");
+
+// изменяемые поля профиля
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+
+// элкменты попапа профиля
+const popup = document.querySelector(".popup");
 
 
-
-let closePopup = function (event) {
-    if (event.target !== event.currentTarget) return
-    popupToggle();
-
-}
+// элкменты попапа фото
+const popupPlace = document.querySelector(".popup-place");
 
 
-let popupToggle = function () {
-    popup.classList.toggle("popup_display-on");
-    if (popup.classList.contains('popup_display-on')) {
-        name.value = profileName.textContent;
-        description.value = profileDescription.textContent;
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+const popupToggle = (event, force=false) => {
+    console.log(event.target.className)
+    const determinePopup = {
+        'profile__edit-button': popup,
+        'profile__add-button': popupPlace,
+        'form__close': event.target.closest('.popup'),
+        'popup popup_display-on': event.target,
+        'form': event.target.closest('.popup'),
+        'popup popup-place popup_display-on': event.target,
+    }
+    const popupAvatar = determinePopup[event.target.className]
+
+    popupAvatar.classList.toggle("popup_display-on");
+    if (force) return false
+
+    popupAvatar.addEventListener('click', closePopup)
+    const closeButton = popupAvatar.querySelector(".form__close");
+    const currentForm = popupAvatar.querySelector(".form");
+    const currentFunction = {
+        'popup popup_display-on': getPopupValues,
+        'popup popup-place popup_display-on': addPhoto
+    }
+    const functionAvatar = currentFunction[popupAvatar.className]
+    const formName = popupAvatar.querySelector(".form__name");
+    const formDescription = popupAvatar.querySelector(".form__description")
+    if (functionAvatar) {
+        currentForm.addEventListener('submit', functionAvatar (event, formName.value, formDescription.value));
+    }
+    closeButton.addEventListener('click', popupToggle);
+    if (popupAvatar.classList.contains('popup_display-on') && !popupAvatar.classList.contains('popup-place')) {
+        formName.value = profileName.textContent;
+        formDescription.value = profileDescription.textContent;
 }}
 
-
-
-let getPopupValues = function (event) {
-    event.preventDefault()
-
-    let headerInfo = {
-        'name': profileName,
-        'description': profileDescription,
-    };
-    
-    for (let i = 0;  i < inputName.length; i++) {
-        headerInfo[inputName[i].name].textContent = inputName[i].value;
+const addPhoto = (event, name, photo, ) =>  {
+    if (event) {
+       event.preventDefault()
     }
-    popupToggle();
+    const protoElement = document.querySelector("#element-template").content;
+    const element =  protoElement.cloneNode(true);
+    const image = element.querySelector(".element__image");
+    image.src = photo;
+    image.alt = name;
+    element.querySelector(".element__caption").textContent = name;
+    elements.append(element);
+    if (event.type === 'submit') {
+       popupToggle(event, true)
+    }
+}
+initialCards.forEach(element => addPhoto(false, element.name, element.link));
 
+const getPopupValues =  (event, name, description, ) => {
+    console.log(name)
+    console.log(description)
+    console.log(event)
+    event.preventDefault()
+    profileName.textContent = name;
+    profileDescription.textContent = description;
+
+    popupToggle(event, true);
 }
 
-form.addEventListener('submit', getPopupValues);
-openButton.addEventListener('click', popupToggle);
-closeButton.addEventListener('click', popupToggle);
-popup.addEventListener('click', closePopup)
+const closePopup = event => {
+    if (event.target !== event.currentTarget) return
+    popupToggle(event);
+}
+
+
+openButton.addEventListener('click', popupToggle (event));
+addButton.addEventListener('click', popupToggle (event))

f41064ea26b9c54eeb988134164b72d17afd82e8
Georgiy Kuvykin <gedrargerum4@gmail.com>
2020-09-03 23:09:51 +0300

    fix : перепсал js, submit отрабатвыфает по дефолту

--- scripts/script.js
+++ scripts/script.js
@@ -3,17 +3,17 @@
 const elements = document.querySelector(".elements");
 
 // кнопка редактирования профиля
-const openButton = document.querySelector(".profile__edit-button");
+const profileButton = document.querySelector(".profile__edit-button");
 
 // кнопка добавления фото
-const addButton = document.querySelector(".profile__add-button");
+const placeButton = document.querySelector(".profile__add-button");
 
 // изменяемые поля профиля
 const profileName = document.querySelector(".profile__name");
 const profileDescription = document.querySelector(".profile__description");
 
 // элкменты попапа профиля
-const popup = document.querySelector(".popup");
+const popupProfile = document.querySelector(".popup");
 
 
 // элкменты попапа фото
@@ -47,44 +47,8 @@ const initialCards = [
     }
 ];
 
-const popupToggle = (event, force=false) => {
-    console.log(event.target.className)
-    const determinePopup = {
-        'profile__edit-button': popup,
-        'profile__add-button': popupPlace,
-        'form__close': event.target.closest('.popup'),
-        'popup popup_display-on': event.target,
-        'form': event.target.closest('.popup'),
-        'popup popup-place popup_display-on': event.target,
-    }
-    const popupAvatar = determinePopup[event.target.className]
 
-    popupAvatar.classList.toggle("popup_display-on");
-    if (force) return false
-
-    popupAvatar.addEventListener('click', closePopup)
-    const closeButton = popupAvatar.querySelector(".form__close");
-    const currentForm = popupAvatar.querySelector(".form");
-    const currentFunction = {
-        'popup popup_display-on': getPopupValues,
-        'popup popup-place popup_display-on': addPhoto
-    }
-    const functionAvatar = currentFunction[popupAvatar.className]
-    const formName = popupAvatar.querySelector(".form__name");
-    const formDescription = popupAvatar.querySelector(".form__description")
-    if (functionAvatar) {
-        currentForm.addEventListener('submit', functionAvatar (event, formName.value, formDescription.value));
-    }
-    closeButton.addEventListener('click', popupToggle);
-    if (popupAvatar.classList.contains('popup_display-on') && !popupAvatar.classList.contains('popup-place')) {
-        formName.value = profileName.textContent;
-        formDescription.value = profileDescription.textContent;
-}}
-
-const addPhoto = (event, name, photo, ) =>  {
-    if (event) {
-       event.preventDefault()
-    }
+const addPhoto = (name, photo) =>  {
     const protoElement = document.querySelector("#element-template").content;
     const element =  protoElement.cloneNode(true);
     const image = element.querySelector(".element__image");
@@ -92,28 +56,53 @@ const addPhoto = (event, name, photo, ) =>  {
     image.alt = name;
     element.querySelector(".element__caption").textContent = name;
     elements.append(element);
-    if (event.type === 'submit') {
-       popupToggle(event, true)
-    }
 }
-initialCards.forEach(element => addPhoto(false, element.name, element.link));
+initialCards.forEach(element => addPhoto(element.name, element.link));
 
-const getPopupValues =  (event, name, description, ) => {
-    console.log(name)
-    console.log(description)
-    console.log(event)
-    event.preventDefault()
+const getPopupValues =  (name, description) => {
     profileName.textContent = name;
     profileDescription.textContent = description;
-
-    popupToggle(event, true);
 }
+
+
+let popupToggle = function (event, popup) {
+    const closeButton = popup.querySelector(".form__close");
+    const currentForm = popup.querySelector(".form");
+    const formName = popup.querySelector(".form__name");
+    const formDescription = popup.querySelector(".form__description")
+    closeButton.addEventListener('click', closePopup);
+    popup.addEventListener('click', closePopup)
+    if (popup.classList.contains('popup_display-on')) {
+        let buttonFunction;
+        if (!popup.classList.contains('popup-place') && popup.classList.contains('popup')) buttonFunction = getPopupValues
+        if (popup.classList.contains('popup-place')) buttonFunction = addPhoto
+        currentForm.addEventListener('submit', function(event){
+            event.preventDefault();
+            buttonFunction(formName.value, formDescription.value);
+            popup.classList.toggle("popup_display-on");
+        })
+        if (!popup.classList.contains('popup-place')) {
+            formName.value = profileName.textContent;
+            formDescription.value = profileDescription.textContent;
+        }
+    }
+
+    popup.classList.toggle("popup_display-on");
+    }
+
+
+const profileDetermineFunction = event => popupToggle(event, popupProfile)
+
+
+const placeDetermineFunction = event => popupToggle(event, popupPlace)
+
 
 const closePopup = event => {
     if (event.target !== event.currentTarget) return
-    popupToggle(event);
+    let popup = event.target.closest('.popup');
+    popupToggle(event, popup);
 }
 
 
-openButton.addEventListener('click', popupToggle (event));
-addButton.addEventListener('click', popupToggle (event))
+profileButton.addEventListener('click', profileDetermineFunction);
+placeButton.addEventListener('click', placeDetermineFunction)

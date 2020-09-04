038ff76463b79e3c5da9ef6c65fd25119f187571
kga <kbuf,kzltq>
2020-09-04 12:08:42 +0300

    fix: разобрался с обработчиками

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,3 @@
-
 // список элементов картинок
 const elements = document.querySelector(".elements");
 
@@ -14,11 +13,12 @@ const profileDescription = document.querySelector(".profile__description");
 
 // элкменты попапа профиля
 const popupProfile = document.querySelector(".popup");
+const closeButtonProfile = popupProfile.querySelector(".form__close");
 
 
 // элкменты попапа фото
 const popupPlace = document.querySelector(".popup-place");
-
+const closeButtonPlace = popupPlace.querySelector(".form__close");
 
 const initialCards = [
     {
@@ -49,7 +49,6 @@ const initialCards = [
 
 
 const addPhoto = (name, photo) => {
-    console.log('dsfffffffffff')
     const protoElement = document.querySelector("#element-template").content;
     const element =  protoElement.cloneNode(true);
     const image = element.querySelector(".element__image");
@@ -65,50 +64,51 @@ const getPopupValues =  (name, description) =>  {
     profileDescription.textContent = description;
 }
 
-
-const profileDetermineFunction = event  => popupToggle(event, popupProfile);
-const placeDetermineFunction = event => popupToggle(event, popupPlace);
-
 const determineFunction =  popup =>  {
     if (!popup.classList.contains('popup-place') && popup.classList.contains('popup'))  return getPopupValues;
     if (popup.classList.contains('popup-place')) return  addPhoto;
 }
 
+const popupToggle = function (popup) {
+    popup.classList.toggle("popup_display-on");
+    if (popup.classList.contains('popup_display-on') && !popup.classList.contains('popup-place')) {
+        popup.querySelector(".form__name").value = profileName.textContent;
+        popup.querySelector(".form__description").value = profileDescription.textContent;
+    }
+}
+const profileDetermineFunction = ()  => popupToggle(popupProfile);
 
+const placeDetermineFunction = () => popupToggle(popupPlace);
 
 const closePopup = event =>  {
     if (event.target !== event.currentTarget) return
     let popup = event.target.closest('.popup');
-    popupToggle(event, popup);
+    popupToggle(popup);
 }
 
-let popupToggle = function (event, popup) {
-    popup.classList.toggle("popup_display-on");
-    const closeButton = popup.querySelector(".form__close");
-    const currentForm = popup.querySelector(".form");
+const popupHandler = (event, popup) => {
     const formName = popup.querySelector(".form__name");
     const formDescription = popup.querySelector(".form__description")
-    closeButton.addEventListener('click', closePopup);
-    popup.addEventListener('click', closePopup)
-    let buttonFunction = determineFunction(popup)
-    if (popup.classList.contains('popup_display-on')) {
-
-        currentForm.addEventListener('submit', function(event){
-
-
-            buttonFunction(formName.value, formDescription.value);
-            popup.classList.remove("popup_display-on")
-            event.preventDefault();
-        })
-
-        if (!popup.classList.contains('popup-place')) {
-            formName.value = profileName.textContent;
-            formDescription.value = profileDescription.textContent;
-        }
-    }
+    const popup_function = determineFunction(popup)
+    popup_function(formName.value, formDescription.value);
+    popup.classList.remove("popup_display-on");
+    event.preventDefault();
 }
 
 
+popupProfile.addEventListener('submit', function(event){
+    popupHandler(event, popupProfile);
+})
 
+popupPlace.addEventListener('submit', function(event){
+    popupHandler(event, popupPlace);
+})
+
+popupProfile.addEventListener('click', closePopup)
+popupPlace.addEventListener('click', closePopup)
+popupProfile.addEventListener('click', closePopup);
+popupPlace.addEventListener('click', closePopup);
 profileButton.addEventListener('click', profileDetermineFunction);
 placeButton.addEventListener('click', placeDetermineFunction);
+closeButtonProfile.addEventListener('click', closePopup);  
+closeButtonPlace.addEventListener('click', closePopup);  

bb76faeaed0f55460a58104f92dd171499708665
Georgiy Kuvykin <gedrargerum4@gmail.com>
2020-09-10 00:45:17 +0300

    fix: вынесены константы, работает фото

---
+++ scripts/constants.js
@@ -0,0 +1,26 @@
+export const initialCards = [
+    {
+        name: 'Архыз',
+        description: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        description: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        description: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        description: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        description: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        description: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+import {initialCards} from './constants.js';
+
 // список элементов картинок
 const gallery = document.querySelector(".elements");
 
@@ -13,7 +15,8 @@ const profileDescription = document.querySelector(".profile__description");
 
 // элкменты попапа профиля
 const popupProfile = document.querySelector(".popup_fio");
-
+const profilePopupName = popupProfile.querySelector('.popup__name');
+const profilePopupDescription = popupProfile.querySelector('.popup__description');
 
 // элкменты попапа объектов галерии
 const popupPlace = document.querySelector(".popup_place");
@@ -23,78 +26,29 @@ const popupPhoto = document.querySelector(".popup_photo-position")
 // Шаблон фото
 const protoElement = document.querySelector("#element-template").content;
 
-// Очень странно, я сделал еще один файл js, поместил в него массив фотографий.
-// объявил вот так: export const initialCards = [...]. В этом файле пытался импортировать
-// import {initialCards} from "./constants.js" , но JS бы против, говорил что файл не яв-ся модулем.
-// Я в html заделал ему type=module", и сломалось вообще.
-
-
-const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-];
-
-const afterClose = (popup) => {
-    if (popup === popupProfile) {
-        popup.querySelector(".popup__name").value = profileName.textContent;
-        popup.querySelector(".popup__description").value = profileDescription.textContent;
-    }
-}
-
-
 const closePopup = popup => {
     popup.classList.remove("popup_display-on")
 
     removeListeners(popup);
 }
 
-// пока пришло в голову только это, но это из быстрого решения. Я так пологаю вы хотели что бы я
-// объявил все внутренности попапов отдельно. И в зависимости от запуска или закрытия попапа,
-// был бы свой уникальный колбэк, внутри которого вызывалась бы общая функция закрытия?
-const closeWrapper = (popup) => {
-    closePopup(popup);
-    afterClose(popup);
-}
-
 
 const overlayClose = event => {
     if (event.target !== event.currentTarget) return
     const popup = event.target.closest(".popup");
-    closeWrapper(popup);
+    closePopup(popup);
 }
 
 const escHandler = event => {
     if (event.key === 'Escape') {
         const activePopup = document.querySelector('.popup_display-on');
-        closeWrapper(activePopup);
+        closePopup(activePopup);
     }
 }
 
 const btnClosePopup = event => {
     const popup = event.target.closest(".popup");
-    closeWrapper(popup);
+    closePopup(popup);
 }
 
 const removeListeners = popup => {
@@ -162,7 +116,7 @@ const getCardElement = (arrayElement) => {
     const caption = element.querySelector(".element__caption")
     const delButton = element.querySelector(".element__delete")
     const likeButton = element.querySelector(".element__like")
-    image.src = arrayElement.link;
+    image.src = arrayElement.description;
     image.alt = arrayElement.name;
     caption.textContent = arrayElement.name;
     image.addEventListener('click', popupPhotoAction);
@@ -173,9 +127,9 @@ const getCardElement = (arrayElement) => {
 
 initialCards.forEach(element => addPhoto(element));
 
-const getPopupValues = (name, description) => {
-    profileName.textContent = name;
-    profileDescription.textContent = description;
+const getPopupValues = (popupInput) => {
+    profileName.textContent = popupInput.name;
+    profileDescription.textContent = popupInput.description;
 }
 
 const determineFunction = popup => {
@@ -195,13 +149,23 @@ const popupHandler = (event) => {
     const formDescription = popup.querySelector(".popup__description");
     const popupFunction = determineFunction(popup);
     if (!popupFunction) return
-    popupFunction(formName.value, formDescription.value);
+    popupFunction({'name' : formName.value, 'description' :formDescription.value});
     closePopup(popup);
 }
 
-profileButton.addEventListener('click', popupProfileAction);
-placeButton.addEventListener('click', popupPlaceAction);
 
-// может я не правильно полял, просто в тренажере было сказано о том, что много слушателей на элементаж ест много памяти
-// я только по этому решил сделшать так. Можете объяснить в какой ситуации вешать слушатель на всю область, или,
-// а в какой вешать на каждый элемент
+const beforeOpen = () => {
+    profilePopupName.value = profileName.textContent;
+    profilePopupDescription.value = profileDescription.textContent;
+}
+
+
+profileButton.addEventListener('click', () => {
+    // я конечно думал об этом, но предыдущий ревьер предложил это сделать после, тем более что еще был
+    // метод toggle. Спасибо за ревью удачи)
+    // P.S не нашел способо из коробки запустить константы из модуля, вышло только когда я установил локальный сервак.
+
+    beforeOpen()
+    popupProfileAction()
+} );
+placeButton.addEventListener('click', popupPlaceAction);
